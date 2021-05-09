20540ab45168b4b54fa99a67c58bfcaa47597c25
tuiretuominen <58442768+tuiretuominen@users.noreply.github.com>
2021-05-09 19:39:48 +0300

    Cleaning file

--- testproject/src/main/java/Test.java
+++ testproject/src/main/java/Test.java
@@ -12,19 +12,8 @@
 // here is 6 lines
 
 // It is sunday today
-=======
-// first testline
-
-// Tuire testing
-
-// New test
-
-// Hellou!
-
-// New test
 
 //29.4.2021 test
 // Testing again
 
-// It is sunday today
 
